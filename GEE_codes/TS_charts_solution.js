a9411e1b036790767694c8930ccbfa37091d2bb9
Daniel Paluba <33784015+palubad@users.noreply.github.com>
2023-06-25 10:36:11 +0200

    Update TS_charts_solution.js

--- GEE_codes/TS_charts_solution.js
+++ GEE_codes/TS_charts_solution.js
@@ -19,6 +19,10 @@ Note: This is the "solution" code - how the final should look like.
 
 © This code is published under MIT License: 
 https://github.com/palubad/TAT2023/blob/main/LICENSE
+
+Contacts:
+email: palubad@natur.cuni.cz
+LinkedIn: https://www.linkedin.com/in/daniel-paluba/
 */
 
 // ========================================================================================
